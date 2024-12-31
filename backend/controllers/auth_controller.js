9320f854fb25414fb6160cb470f5d258ad6ba258
Jahirul Islam <talk2jahir.official@gmail.com>
2024-12-31 15:39:49 +0600

    Sign up api complete

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -1,9 +1,75 @@
 import express from 'express';
+import User from '../models/user.js';
+import bcrypt from 'bcryptjs'
+import { generateTokenAndSetCookie } from '../lib/utils/generateToken.js';
 
 export const signUp = async (req, res) => {
-    res.json({
-        data: "You hit the signup endpoint"
-    });
+    try {
+        const { username, fullName, password, email } = req.body;
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({
+                message: "Invalid email format",
+            });
+        }
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({
+                message: "Username already exists",
+            });
+        }
+
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({
+                message: "Email already exists",
+            });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({
+                message: "Password must be at least 6 characters",
+            });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            username,
+            fullName,
+            email,
+            password: hashedPassword,
+        });
+
+        await newUser.save();
+
+        if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res);
+            console.log("New user: ", newUser);
+            res.status(201).json({
+                _id: newUser._id,
+                username: newUser.username,
+                email: newUser.email,
+                followers: newUser.followers,
+                following: newUser.following,
+                profileImg: newUser.profileImg,
+                coverImg: newUser.coverImg,
+            });
+        } else {
+            res.status(400).json({
+                message: "Invalid user data",
+            });
+        }
+
+    } catch (error) {
+        console.log("Error on sign up controller: ", error);
+        res.status(500).json({
+            message: "Internal Server Error.",
+        });
+    }
 };
 
 export const logIn = async (req, res) => {
@@ -17,4 +83,3 @@ export const logOut = async (req, res) => {
         data: "You hit the logout endpoint"
     });
 };
-
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
 
 
