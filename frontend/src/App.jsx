e514a23a5bddb367f8a0e91bb60caea69f49bad3
Jahirul Islam <talk2jahir.official@gmail.com>
2025-01-22 11:37:58 +0600

    login complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Sidebar from './components/common/Sidebar'
 import RightPanel from './components/common/RightPanel'
 import NotificationPage from './pages/notifications/NotificationPage'
 import ProfilePage from './pages/profile/ProfilePage'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,6 +24,7 @@ function App() {
           <Route path="/profile/johndoe" element={<ProfilePage />} />
         </Routes>
         <RightPanel />
+        <Toaster />
       </div>
     </>
   )
